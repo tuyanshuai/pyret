721063e2fb4119b046ac78f491c93679afafce02
Yanshuai Tu <tuyanshuai@gmail.com>
2020-06-13 06:48:16 -0700

    add

--- test.py
+++ test.py
@@ -6,9 +6,9 @@ import pymesh as pymesh
 epi_img = nib.load('data/testdata.nii.gz')
 epi_img_data = epi_img.get_fdata()
 
+# test gii
 
-# test mesh plot
+import nibabel as nib
+from nibabel.testing import test_data
 
-mesh = pymesh.load_mesh("model.obj")
-
-
+surf_img = nib.load(test_data('gifti', 'ascii.gii'))
