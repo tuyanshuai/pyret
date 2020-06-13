32e12d84b9f0395ccba95296115bcf8947895ff0
Yanshuai Tu <tuyanshuai@gmail.com>
2020-06-13 07:05:56 -0700

    todo: Fix plot_mesh

--- graphics/plot_mesh.py
+++ graphics/plot_mesh.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.tri as mtri
 def plot_mesh(F, V, color = None):
-    fig = plt.figure(figsize=plt.figaspect(0.5))
-    ax = fig.add_axes()
-    ax.plot_trisurf(V[0],V[1],V[2], triangles=F, cmap=plt.cm.Spectral)
+    fig = plt.figure()
+    ax = fig.gca()
+    ax.plot_trisurf(V[:, 0], V[:, 1], V[:, 2], triangles=F, cmap=plt.cm.Spectral)
+    plt.show()
